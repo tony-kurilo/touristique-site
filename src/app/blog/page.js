07ffe7c966597ab8fba4c1672403c98e62496cc1
Tony Kurilo <lang.nattreise@gmail.com>
2024-10-17 15:53:52 +0300

    contact-us page done

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,3 +1,26 @@
-export default function Page() {
+import Link from 'next/link';
 
-}
+const BlogPage = () => {
+    const articles = [
+        { id: 1, title: 'Первая новость' },
+        { id: 2, title: 'Вторая новость' },
+        // Другие статьи
+    ];
+
+    return (
+        <div>
+            <h1>Новости</h1>
+            <ul>
+                {articles.map(article => (
+                    <li key={article.id}>
+                        <Link href={`/blog/${article.id}`}>
+                            {article.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default BlogPage;
